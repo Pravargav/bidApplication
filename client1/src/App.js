860dd5aa6d74160f2be0311169afe5419739a55b
Pravargav <96449025+Pravargav@users.noreply.github.com>
2023-05-24 18:33:35 +0530

    oerogsos
    hhoogjgi

---
+++ client1/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Bid } from "./pages/bid/bid"
+import { ShopContextProvider } from "./context/shop-context";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+
+             <Router>
+               <Navbar />
+               <Routes>
+                 <Route path="/" element={<Shop />} />
+                 <Route path="/bid" element={<Bid />} />
+               </Routes>
+             </Router>
+
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
